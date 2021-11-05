eb3f499f9f2d2d1d7f65e5e77ea41306ee99f35c
xxxJotaxxx <joelruizlopez6@gmail.com>
2021-11-05 22:11:11 +0100

    display_7

---
+++ src/main.cpp
@@ -0,0 +1,49 @@
+#include <Arduino.h>
+
+int pausa = 1000; //interval pause
+
+void setup()
+{
+    pinMode(7, OUTPUT);
+    pinMode(8, OUTPUT);
+    pinMode(9, OUTPUT);
+    pinMode(10, OUTPUT);
+    pinMode(11, OUTPUT);
+    pinMode(12, OUTPUT);
+    pinMode(13, OUTPUT);
+}
+
+void display(int a, int b, int c, int d, int e, int f, int g)
+{
+    digitalWrite(7, a);
+    digitalWrite(8, b);
+    digitalWrite(9, c);
+    digitalWrite(10, d);
+    digitalWrite(11, e);
+    digitalWrite(12, f);
+    digitalWrite(13, g);
+}
+
+void loop()
+{
+    display(1, 1, 1, 1, 1, 1, 0); //write 0
+    delay(pausa);
+    display(0, 1, 1, 0, 0, 0, 0); //write 1
+    delay(pausa);
+    display(1, 1, 0, 1, 1, 0, 1); //write 2
+    delay(pausa);
+    display(1, 1, 1, 1, 0, 0, 1); //write 3
+    delay(pausa);
+    display(0, 1, 1, 0, 0, 1, 1); //write 4
+    delay(pausa);
+    display(1, 0, 1, 1, 0, 1, 1); //write 5
+    delay(pausa);
+    display(1, 0, 1, 1, 1, 1, 1); //write 6
+    delay(pausa);
+    display(1, 1, 1, 0, 0, 0, 0); //write 7
+    delay(pausa);
+    display(1, 1, 1, 1, 1, 1, 1); //write 8
+    delay(pausa);
+    display(1, 1, 1, 0, 0, 1, 1); //write 9
+    delay(pausa);
+}
